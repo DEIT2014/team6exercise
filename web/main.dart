466a7d4767b5ae759fb41ee5a4d9890d72d2a490
lzhiwei <18717983956@163.com>
2016-11-17 15:12:34 +0800

    合并
    将教师评价的页面合并到index里面，新建doc文档

--- web/main.dart
+++ web/main.dart
@@ -7,3 +7,14 @@ void main() {
   querySelector('#output').text = 'WEB APP IS RUNNING!';
 
 }
+
+void main() {
+  querySelector('#sample_text_id')
+    ..text = 'Click me!'
+    ..onClick.listen(reverseText);
+}
+/// reverseText用来接受用户点击按钮翻转字符的响应工作。
+/// 参数[event]是鼠标事件....
+void reverseText(MouseEvent event) {
+  //todo 在这里添加代码处理鼠标点击之后的工作。
+}
